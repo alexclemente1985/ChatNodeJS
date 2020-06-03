f84cead40074c168da28b8f6005e478fcb051aaa
alexandre_pinheiro85 <alexandre.pinheiro@afferolab.com.br>
2020-06-03 17:29:25 -0300

    Version 1.1.0

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,9 +1,9 @@
 module.exports = (application)=>{
     application.get('/chat',(req,res)=>{
-        application.app.controllers.chat.chatStart(application,req,res);
+        application.controllers.chat.chatStart(application,req,res);
     });
 
     application.post('/chat',(req,res)=>{
-        application.app.controllers.chat.chatStart(application,req,res);
+        application.controllers.chat.chatStart(application,req,res);
     });
 }
